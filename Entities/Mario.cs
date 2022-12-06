8970071e4bae9a2028b13c97e02b53b9f078b505
BuenoIT <101655079+BuenoIT@users.noreply.github.com>
2022-12-06 13:40:47 -0500

    Mario Sprint Jumping Changed

--- Entities/Mario.cs
+++ Entities/Mario.cs
@@ -12,10 +12,21 @@ namespace RunnerByMarioGame.Entities
 {
     internal class Mario : IMarioRunnerEntity
     {
+        
+        //Mario Iddle Sprite
         public int _mario_sprite_X = 12;
         public int _mario_sprite_Y = 18;
         public int _mario_sprite_width = 72;
         public int _mario_sprite_height = 96;
+
+        //Mario Jump Sprite
+        public int _mario_jump_sprite_X = 450;
+        public int _mario_jump_sprite_Y = 18;
+        public int _mario_jump_sprite_width = 96;
+        public int _mario_jump_sprite_height = 96;
+
+
+        //Jump variables declaration and initialization
         Vector2 position = new Vector2(1, 250); //Initial Position Mario
         Vector2 velocity;
 
@@ -35,12 +46,22 @@ namespace RunnerByMarioGame.Entities
 
         public void Draw(SpriteBatch spriteBatch, GameTime gameTime)
         {
+            
             if (MarioState == MarioState.Idle)
             {
+                MarioSprite.PointX = _mario_sprite_X;
+                MarioSprite.PointY= _mario_sprite_Y;
+                MarioSprite.Width= _mario_sprite_width;
+                MarioSprite.Height= _mario_sprite_height;
                 MarioSprite.Draw(spriteBatch, MarioPosition);
             }
             else if (MarioState == MarioState.JumpingUp)
             {
+                MarioSprite.PointX = _mario_jump_sprite_X;
+                MarioSprite.PointY = _mario_jump_sprite_Y;
+                MarioSprite.Width = _mario_jump_sprite_width;
+                MarioSprite.Height = _mario_jump_sprite_height;
+
                 MarioSprite.Draw(spriteBatch, MarioPosition);
             }
            
@@ -48,9 +69,7 @@ namespace RunnerByMarioGame.Entities
 
         public void Update(GameTime gameTime)
         {
-
             Jump();
-
         }
 
         public void Jump()
@@ -60,7 +79,7 @@ namespace RunnerByMarioGame.Entities
             if (Keyboard.GetState().IsKeyDown(Keys.Space) && MarioState != MarioState.JumpingUp)
             {
                 position.Y -= 10f;
-                velocity.Y = -7f;
+                velocity.Y = -8f;
                 MarioState = MarioState.JumpingUp;
             }
 
@@ -81,5 +100,6 @@ namespace RunnerByMarioGame.Entities
 
             MarioPosition = position;
         }
+
     }
 }
