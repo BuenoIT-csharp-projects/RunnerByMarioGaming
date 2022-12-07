e6115e83e5450363d26d16510253fb2d79d41558
Sergio Toledo <sergio.augusto.toledo@gmail.com>
2022-12-07 18:28:45 -0500

    Background implementation

--- MarioRunner.cs
+++ MarioRunner.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using RunnerByMarioGame.Background;
 using RunnerByMarioGame.Entities;
 using RunnerByMarioGame.Sprites;
 
@@ -26,6 +27,9 @@ namespace RunnerByMarioGame
         public const int screen_width = 1000;
         public const int screen_height = 400;
 
+        //Background scrolling declaration
+        private Scrolling _scrolling1;
+        private Scrolling _scrolling2;
 
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
@@ -53,6 +57,10 @@ namespace RunnerByMarioGame
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
+            //Background movement
+            _scrolling1 = new Scrolling(Content.Load<Texture2D>("background"), new Rectangle(0, -300, 1280, 720));
+            _scrolling2 = new Scrolling(Content.Load<Texture2D>("background"), new Rectangle(1280, -300, 1280, 720));
+
             //Load Mario Sprite to the Game
             _spriteMario = Content.Load<Texture2D>(mario_asset_img);
             _mario = new Mario(_spriteMario, new Vector2(1, 250)); //Hard code initialization position of Mario to the game
@@ -68,7 +76,19 @@ namespace RunnerByMarioGame
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
-            // TODO: Add your update logic here
+            //Background Image position update
+            if (_scrolling1.rectangle.X + 1280 <= 0)
+            {
+	            _scrolling1.rectangle.X = _scrolling2.rectangle.X + 1280;
+            }
+            if (_scrolling2.rectangle.X + 1280 <= 0)
+            {
+	            _scrolling2.rectangle.X = _scrolling1.rectangle.X + 1280;
+            }
+
+            _scrolling1.Update();
+            _scrolling2.Update();
+
             _mario.Update(gameTime);
             _goomba.Update(gameTime);
 
@@ -80,6 +100,10 @@ namespace RunnerByMarioGame
             GraphicsDevice.Clear(Color.White);
 
             _spriteBatch.Begin();
+
+            //Background draw
+            _scrolling1.Draw(_spriteBatch);
+            _scrolling2.Draw(_spriteBatch);
 
             _mario.Draw(_spriteBatch, gameTime);
             _goomba.Draw(_spriteBatch, gameTime);
